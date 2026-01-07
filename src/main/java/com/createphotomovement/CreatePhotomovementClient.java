61b1b8cb8342a56aef2ed97a11471905bb8a363d
Nedzad Winter <97174749+dazden94@users.noreply.github.com>
2026-01-07 21:32:41 +0100

    first draft

--- src/main/java/com/createphotomovement/CreatePhotomovementClient.java
+++ src/main/java/com/createphotomovement/CreatePhotomovementClient.java
@@ -1,14 +1,23 @@
 package com.createphotomovement;
 
 import com.createphotomovement.content.kinetics.solargenerator.SolarGeneratorRenderer;
-import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
+import com.createphotomovement.ponder.PhotomovementPonderPlugin;
+
+import net.createmod.ponder.foundation.PonderIndex;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
 @EventBusSubscriber(modid = CreatePhotomovement.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class CreatePhotomovementClient {
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        // Register Ponder plugin
+        PonderIndex.addPlugin(new PhotomovementPonderPlugin());
+    }
 
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
